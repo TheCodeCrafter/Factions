a38c5d0041d7e9fe7ced82a683406ab12a1c91ef
Noah <noahcoetsee@gmail.com>
2018-02-05 15:24:51 -0500

    Create FocusFile.java

---
+++ src/main/java/com/massivecraft/factions/util/FocusFile.java
@@ -0,0 +1,42 @@
+package com.massivecraft.factions.util;
+
+import org.bukkit.configuration.file.YamlConfiguration;
+import net.fathomtech.plugins.CityPlus.Main;
+import java.io.File;
+
+@SuppressWarnings("unused")
+public class FocusFile {
+
+    private final Main main;
+    public final YamlConfiguration data = new YamlConfiguration();
+    private final File focusFile;
+
+    public SMSFile(Main main) {
+        this.main = main;
+        focusFile = new File(main.getDataFolder(), "focus.yml");
+        if (!focusFile.exists())
+            try {
+                focusFile.createNewFile();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        load();
+    }
+
+    public void save() {
+        try {
+            data.save(focusFile);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void load() {
+        try {
+            data.load(focusFile);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+}
